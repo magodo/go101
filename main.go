3d8b7430b6ac6e4e99bd0aa39b9e2054bb1da9c5
magodo <wztdyl@sina.com>
2020-03-12 14:38:46 +0800

    echo tcp server

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const (
+	ServerAddr    = "localhost"
+	ServerPort    = 12345
+	ServerNetwork = "tcp"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	l, err := net.Listen(ServerNetwork, fmt.Sprintf("%s:%d", ServerAddr, ServerPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	io.Copy(conn, conn)
 }
